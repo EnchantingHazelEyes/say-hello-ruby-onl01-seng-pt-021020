d7b862d2e6874ac9a6fa2de5c7d5331c0b1534c7
Samantha Barros <samantha.barros95@gmail.com>
2020-02-12 04:29:01 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,6 @@
 # Build your say_hello method here
-def say_hello(namee="Ruby Programmer")
-  say_hello("Gabriela")
+def say_hello(name="Ruby Programmer")
   puts "Hello #{name} !"
 end
+
+say_hello("Gabriela")
